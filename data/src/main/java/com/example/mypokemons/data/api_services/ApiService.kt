0a3ef43942dd3f249b4ba275826ee8b734195808
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-19 05:40:45 +0300

    Добавляю 1 запрос на поиск карточек по имени

--- data/src/main/java/com/example/mypokemons/data/api_services/ApiService.kt
+++ data/src/main/java/com/example/mypokemons/data/api_services/ApiService.kt
@@ -1,27 +1,28 @@
-package com.example.mypokemons.data.ApiServices
+package com.example.mypokemons.data.api_services
 
-import com.example.mypokemons.data.storage.PokemonsDataModel
+import com.example.mypokemons.data.storage.PokemonModel
 import io.reactivex.Observable
+import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
-    @GET("v2/sets")
-    fun getAllCards(): Observable<PokemonsDataModel>
+    @GET("v2/cards/")
+    fun getAllCards(): Single<PokemonModel>
+
+    @GET("/v2/cards?q=set.name:{name}")
+    fun getFoundCards(@Path("name") name: String): Single<PokemonModel>
 
     companion object {
-        val retrofitService by lazy {
+        val apiService by lazy {
             Retrofit.Builder()
                 .baseUrl("https://api.pokemontcg.io/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-        }
-
-        val api: ApiService by lazy {
-            retrofitService.create(ApiService::class.java)
+                .build().create(ApiService::class.java)
         }
     }
 }
