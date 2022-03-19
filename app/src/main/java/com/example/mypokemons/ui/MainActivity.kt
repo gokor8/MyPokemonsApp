1236bcdc31b17ac9be2834675f2f1fd4586d43ea
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-19 05:41:23 +0300

    Изменяю базовую активность под тулбар и добавляю к ней вбю модель

--- app/src/main/java/com/example/mypokemons/ui/MainActivity.kt
+++ app/src/main/java/com/example/mypokemons/ui/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mypokemons.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mypokemons.R
 import com.example.mypokemons.databinding.ActivityMainBinding
+import com.example.mypokemons.view_models.MainViewModel
+import com.example.mypokemons.view_models.PokemonCardsViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
                 as NavHostFragment
 
         binding.bnvBase.setupWithNavController(navHostFragment.navController)
+
+        binding.toolbar.ivBack.setOnClickListener {
+
+        }
+
+        binding.toolbar.searchButton.setOnClickListener {
+
+        }
     }
 }
