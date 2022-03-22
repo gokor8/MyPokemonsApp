14a2de5cab0d5ffeba0395fddf57785cdfb1e690
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-23 01:49:20 +0300

    Накидные изменения, для примерной разметки методов и их возвращаемых типов

--- data/src/main/java/com/example/mypokemons/data/database/room/dao/PokemonDao.kt
+++ data/src/main/java/com/example/mypokemons/data/database/room/dao/PokemonDao.kt
@@ -1,10 +1,12 @@
 package com.example.mypokemons.data.database.room.dao
 
 import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
 import com.example.mypokemons.data.database.room.PokemonEntity
 import com.example.mypokemons.data.storage.PokemonInfo
 import io.reactivex.Completable
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
 
 @Dao
@@ -12,10 +14,13 @@ interface PokemonDao {
     @Query("SELECT * FROM pokemons")
     fun getAll(): Single<List<PokemonEntity>>
 
-    @Query("SELECT * FROM pokemons WHERE id = :id")
-    fun getById(id: Int): PokemonEntity
+    @Query("SELECT * FROM pokemons WHERE name = '%' || :name  || '%'")
+    fun getByName(name: String): Observable<List<PokemonEntity>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Query("SELECT * FROM pokemons WHERE name LIKE '%' || :name  || '%'")
+    fun getEqualsByName(name: String): Observable<List<PokemonEntity>>
+
+    @Insert(onConflict = REPLACE)
     fun insertList(pokemons: List<PokemonEntity>)
 
     @Query("SELECT count(*) FROM pokemons")
@@ -24,8 +29,17 @@ interface PokemonDao {
     @Query("DELETE FROM pokemons")
     fun deleteAll()
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = REPLACE)
     fun update(pokemon: PokemonEntity)
+
+    @Query("UPDATE pokemons SET is_favorite = :newFavoriteStatus WHERE name = :name")
+    fun updateFavoriteByName(name: String, newFavoriteStatus: Int): Single<Unit>
+
+    @Query("select * FROM pokemons WHERE name = :name")
+    fun getFavoriteByName(name: String) : Observable<PokemonEntity>
+
+    @Query("select * FROM pokemons WHERE name = :name")
+    fun getCardByName(name: String) : Observable<PokemonEntity>
 
     @Update
     fun updateAll(pokemons: List<PokemonEntity>)
