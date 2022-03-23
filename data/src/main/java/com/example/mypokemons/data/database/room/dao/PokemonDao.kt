f6276dfed1c7a60a7161f69375df2f2d946e97a3
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-23 09:13:23 +0300

    Перетасовываю методы Дао, по их назначению

--- data/src/main/java/com/example/mypokemons/data/database/room/dao/PokemonDao.kt
+++ data/src/main/java/com/example/mypokemons/data/database/room/dao/PokemonDao.kt
@@ -3,14 +3,17 @@ package com.example.mypokemons.data.database.room.dao
 import androidx.room.*
 import androidx.room.OnConflictStrategy.REPLACE
 import com.example.mypokemons.data.database.room.PokemonEntity
-import com.example.mypokemons.data.storage.PokemonInfo
-import io.reactivex.Completable
-import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
 
 @Dao
 interface PokemonDao {
+
+    companion object{
+        const val FAVORITE = 1
+        const val UNFAVORITE = 0
+    }
+
     @Query("SELECT * FROM pokemons")
     fun getAll(): Single<List<PokemonEntity>>
 
@@ -20,26 +23,29 @@ interface PokemonDao {
     @Query("SELECT * FROM pokemons WHERE name LIKE '%' || :name  || '%'")
     fun getEqualsByName(name: String): Observable<List<PokemonEntity>>
 
-    @Insert(onConflict = REPLACE)
-    fun insertList(pokemons: List<PokemonEntity>)
-
     @Query("SELECT count(*) FROM pokemons")
     fun getSize(): Single<Int>
+
+    @Query("SELECT * FROM pokemons WHERE name = :name")
+    fun getFavoriteByName(name: String) : Observable<PokemonEntity>
+
+    @Query("SELECT * FROM pokemons WHERE name = :name")
+    fun getCardByName(name: String) : Observable<PokemonEntity>
+
+    @Query("SELECT * FROM pokemons WHERE is_favorite = :favorite")
+    fun getCardsByFavorite(favorite: Int) : Observable<List<PokemonEntity>>
+
+    @Query("UPDATE pokemons SET is_favorite = :newFavoriteStatus WHERE name = :name")
+    fun updateFavoriteByName(name: String, newFavoriteStatus: Int): Single<Unit>
 
     @Query("DELETE FROM pokemons")
     fun deleteAll()
 
     @Insert(onConflict = REPLACE)
+    fun insertList(pokemons: List<PokemonEntity>)
+
+    @Insert(onConflict = REPLACE)
     fun update(pokemon: PokemonEntity)
-
-    @Query("UPDATE pokemons SET is_favorite = :newFavoriteStatus WHERE name = :name")
-    fun updateFavoriteByName(name: String, newFavoriteStatus: Int): Single<Unit>
-
-    @Query("select * FROM pokemons WHERE name = :name")
-    fun getFavoriteByName(name: String) : Observable<PokemonEntity>
-
-    @Query("select * FROM pokemons WHERE name = :name")
-    fun getCardByName(name: String) : Observable<PokemonEntity>
 
     @Update
     fun updateAll(pokemons: List<PokemonEntity>)
