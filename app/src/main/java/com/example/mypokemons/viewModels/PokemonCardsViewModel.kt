d22a35d8faaa5019dedc9e378bcffde6e448b497
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-03-24 17:35:35 +0300

    Обновляю логику главного окна с покемонами. Для более быстрой загрузки обновлений старого фрагмента, делаю получение данных из базы данных

--- app/src/main/java/com/example/mypokemons/viewModels/PokemonCardsViewModel.kt
+++ app/src/main/java/com/example/mypokemons/viewModels/PokemonCardsViewModel.kt
@@ -8,7 +8,35 @@ import io.reactivex.schedulers.Schedulers
 class PokemonCardsViewModel(application: Application) : BaseViewModel(application) {
     private var model = PokemonsCardsModel(appComponent)
 
+    private var wasDataFromInternet = false
+
     override fun updateData() {
+        if (!wasDataFromInternet) {
+            updateDataFromInternet()
+            wasDataFromInternet = true
+        }
+        else
+            updateDataFromDatabase()
+    }
+
+    private fun updateDataFromDatabase(){
+        val disposeSub = model.getDbCards()
+            .subscribeOn(Schedulers.io())
+            .onErrorResumeNext {
+                Log.e("CardsError", it.message.toString())
+                return@onErrorResumeNext model.getDbCards()
+            }
+            .subscribe({
+                //if(pokemonsLiveData.value != it)
+                pokemonsLiveData.postValue(it)
+            }, {
+                Log.d("RoomError", it.stackTraceToString())
+            }
+            )
+        compositeDisposable.add(disposeSub)
+    }
+
+    private fun updateDataFromInternet() {
         val disposeSub = model.getWebCards()
             .subscribeOn(Schedulers.io())
             .flatMap {
@@ -23,7 +51,7 @@ class PokemonCardsViewModel(application: Application) : BaseViewModel(applicatio
             }
             .subscribe({
                 //if(pokemonsLiveData.value != it)
-                    pokemonsLiveData.postValue(it)
+                pokemonsLiveData.postValue(it)
             }, {
                 Log.d("RoomError", it.stackTraceToString())
             }
